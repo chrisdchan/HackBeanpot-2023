87f5045377a3aff3a67145c9db56534ff93fef09
Chris <christopherdchan02@gmail.com>
2023-02-11 15:20:06 -0500

    add a login and signup

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<h1>Welcome to Our App</h1>
+
+<a href="/signup" class='rounded-md'>
+    Sign Up
+</a>
+
+<a href="/login" class='rounded-md'>
+    Log in
+</a>
+
